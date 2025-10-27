9d64dc6af56edc712469fed6051992aa415e9ffc
Ludwig Wittenberg <lw223cq@student.lnu.se>
2025-10-27 17:51:50 +0100

    Add role serviceAccountUser
    Signed-off-by: Ludwig Wittenberg <lw223cq@student.lnu.se>

--- Terraform/bootstrap/serviceAccount.tf
+++ Terraform/bootstrap/serviceAccount.tf
@@ -31,3 +31,14 @@ resource "google_project_iam_member" "storage_admin" {
 resource "google_service_account_key" "tf_key" {
   service_account_id = google_service_account.tf.name
 }
+
+# Allow Terraform service account to use the default Compute Engine service account
+resource "google_service_account_iam_member" "compute_sa_user" {
+  service_account_id = "projects/${var.project_id}/serviceAccounts/${data.google_compute_default_service_account.default.email}"
+  role               = "roles/iam.serviceAccountUser"
+  member             = "serviceAccount:${google_service_account.tf.email}"
+}
+
+data "google_compute_default_service_account" "default" {
+  project = var.project_id
+}
