c62f69d216f77aac16665d9035f5c59d6ff05803
Ludwig Wittenberg <lw223cq@student.lnu.se>
2025-10-28 09:50:52 +0100

    Modularized Terraform
    Signed-off-by: Ludwig Wittenberg <lw223cq@student.lnu.se>

---
+++ Terraform/Production/variables.tf
@@ -0,0 +1,47 @@
+#############################
+# Google Project Variables
+#############################
+variable "google_project_id" {
+  description = "The id of the project"
+  type = string
+  default = ""
+}
+
+variable "region" {
+  description = "The region of the project"
+  type = string
+  default = ""
+}
+
+#############################
+# Project Variables
+#############################
+variable "project_name" {
+  description = "The name of the project"
+  type = string
+  default = ""
+}
+
+#############################
+# Network Variables
+#############################
+variable "ipv6_access_type" {
+  description = "The access type of the ipv6"
+  type = string
+  default = "INTERNAL"
+}
+
+variable "stack_type" {
+  description = "The stack type of the ipv4/ipv6"
+  type = string
+  default = "IPV4_IPV6"
+}
+
+#############################
+# Cluster Variables
+#############################
+variable "delete_protection" {
+  description = "The delete protection of the cluster"
+  type = bool
+  default = false
+}
---
+++ Terraform/modules/gke/variables.tf
@@ -0,0 +1,23 @@
+variable "google_project_id" {
+  type = string
+}
+
+variable "region" {
+  type = string
+}
+
+variable "project_name" {
+  type = string
+}
+
+variable "ipv6_access_type" {
+  type = string
+}
+
+variable "stack_type" {
+  type = string
+}
+
+variable "delete_protection" {
+  type = bool
+}
