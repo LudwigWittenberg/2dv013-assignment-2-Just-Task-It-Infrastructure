2a99bb37576a861387434be9f13e844790a7457c
Ludwig Wittenberg <lw223cq@student.lnu.se>
2025-10-28 16:06:43 +0100

    wip
    Signed-off-by: Ludwig Wittenberg <lw223cq@student.lnu.se>

--- Terraform/bootstrap/bucket.tf
+++ Terraform/bootstrap/bucket.tf
@@ -1,4 +1,6 @@
 resource "google_storage_bucket" "tfstate" {
+  depends_on = [google_project_service.apis]
+
   name                        = "bucket-${lower(var.project_name)}"
   location                    = "EU"
   uniform_bucket_level_access = true
--- Terraform/bootstrap/serviceAccount.tf
+++ Terraform/bootstrap/serviceAccount.tf
@@ -1,23 +1,31 @@
 # Roles for infra management
 resource "google_project_iam_member" "container_admin" {
+  depends_on = [google_project_service.apis]
+
   project = var.project_id
   role    = "roles/container.admin"
   member  = "serviceAccount:${var.email}"
 }
 
 resource "google_project_iam_member" "network_admin" {
+  depends_on = [google_project_service.apis]
+
   project = var.project_id
   role    = "roles/compute.networkAdmin"
   member  = "serviceAccount:${var.email}"
 }
 
 resource "google_project_iam_member" "artifactregistry_admin" {
+  depends_on = [google_project_service.apis]
+
   project = var.project_id
   role    = "roles/artifactregistry.admin"
   member  = "serviceAccount:${var.email}"
 }
 
 resource "google_project_iam_member" "storage_admin" {
+  depends_on = [google_project_service.apis]
+
   project = var.project_id
   role    = "roles/storage.admin"
   member  = "serviceAccount:${var.email}"
@@ -25,6 +33,8 @@ resource "google_project_iam_member" "storage_admin" {
 
 # Allow Terraform service account to use the default Compute Engine service account
 resource "google_service_account_iam_member" "compute_sa_user" {
+  depends_on = [google_project_service.apis]
+
   service_account_id = "projects/${var.project_id}/serviceAccounts/${data.google_compute_default_service_account.default.email}"
   role               = "roles/iam.serviceAccountUser"
   member             = "serviceAccount:${var.email}"
