2092c0ff9f43b43725cf5d1a505894b22c222a09
Ludwig Wittenberg <lw223cq@student.lnu.se>
2025-10-29 11:28:53 +0100

    wip
    Signed-off-by: Ludwig Wittenberg <lw223cq@student.lnu.se>

--- Terraform/cluster/provider.tf
+++ Terraform/cluster/provider.tf
@@ -11,13 +11,3 @@ provider "google" {
   project = var.google_project_id
   region  = var.region
 }
-
-module "gke" {
-  source      = "../modules/gke"
-  google_project_id  = var.google_project_id
-  region      = var.region
-  project_name = var.project_name
-  ipv6_access_type = var.ipv6_access_type
-  stack_type = var.stack_type
-  delete_protection = var.delete_protection
-}
