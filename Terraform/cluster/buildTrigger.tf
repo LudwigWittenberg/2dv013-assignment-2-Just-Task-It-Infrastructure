6fc4074f489d6b6ccf6504ac15f656f492681121
Ludwig Wittenberg <lw223cq@student.lnu.se>
2025-10-29 11:34:33 +0100

    Dont want to do Cloud Build Yet
    Signed-off-by: Ludwig Wittenberg <lw223cq@student.lnu.se>

--- Terraform/cluster/buildTrigger.tf
+++ Terraform/cluster/buildTrigger.tf
@@ -1,16 +1,16 @@
-resource "google_cloudbuild_trigger" "include-build-logs-trigger" {
-  for_each = toset(var.cluster_name)
-  location = var.region
-  name     = "${lower(var.project_name)}-${each.value}-build-trigger"
-  filename = "cloudbuild.yaml"
+# resource "google_cloudbuild_trigger" "include-build-logs-trigger" {
+#   for_each = toset(var.cluster_name)
+#   location = var.region
+#   name     = "${lower(var.project_name)}-${each.value}-build-trigger"
+#   filename = "cloudbuild.yaml"
 
-  github {
-    owner = "LudwigWittenberg"
-    name  = "terraform-provider-google-beta"
-    push {
-      branch = "^main$"
-    }
-  }
+#   github {
+#     owner = "LudwigWittenberg"
+#     name  = "terraform-provider-google-beta"
+#     push {
+#       branch = "^main$"
+#     }
+#   }
 
-  include_build_logs = "INCLUDE_BUILD_LOGS_WITH_STATUS"
-}
+#   include_build_logs = "INCLUDE_BUILD_LOGS_WITH_STATUS"
+# }
