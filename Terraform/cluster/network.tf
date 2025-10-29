3ea7b7c1f73b3fa530abf9f2d8c46c2973788da0
Ludwig Wittenberg <lw223cq@student.lnu.se>
2025-10-29 11:30:59 +0100

    wip
    Signed-off-by: Ludwig Wittenberg <lw223cq@student.lnu.se>

--- Terraform/cluster/network.tf
+++ Terraform/cluster/network.tf
@@ -18,7 +18,7 @@ resource "google_compute_subnetwork" "default" {
   stack_type       = var.stack_type
   ipv6_access_type = var.ipv6_access_type
 
-  network = google_compute_network.default.id
+  network = google_compute_network.default[each.key].id
   secondary_ip_range {
     range_name    = "services-range"
     ip_cidr_range = "192.168.0.0/24"
