bbdeab578d7483e1d8f98b2efced4f4e27f269cb
Ludwig Wittenberg <lw223cq@student.lnu.se>
2025-10-29 18:48:58 +0100

    Trying without Trigger
    Signed-off-by: Ludwig Wittenberg <lw223cq@student.lnu.se>

--- Terraform/build/trigger.tf
+++ Terraform/build/trigger.tf
@@ -1,14 +1,19 @@
-resource "google_cloudbuild_trigger" "github-trigger" {
-  project           = var.project_id
-  location          = var.region
-  name              = "${var.project_name}-trigger"
-  repository_event_config {
-    repository = google_cloudbuildv2_repository.github.id
-    push {
-      branch = "^main$"
-    }
-  }
-  filename = "cloudbuild.yaml"
+# resource "google_cloudbuild_trigger" "github-trigger" {
+#   project           = var.project_id
+#   location          = var.region
+#   name              = "${var.project_name}-trigger"
+#   repository_event_config {
+#     repository = google_cloudbuildv2_repository.github.id
+#     push {
+#       branch = "main"
+#     }
+#   }
 
-  depends_on = [google_cloudbuildv2_repository.github]
-}
+#   filename = "cloudbuild.yaml"
+
+#   depends_on = [
+#     google_cloudbuildv2_connection.github_connection,
+#     google_cloudbuildv2_repository.github
+#   ]
+
+# }
