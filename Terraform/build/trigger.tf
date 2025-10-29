c56da4998d84c5695afda5cd80ce4d22516cb1e1
Ludwig Wittenberg <lw223cq@student.lnu.se>
2025-10-29 22:00:03 +0100

    wip
    Signed-off-by: Ludwig Wittenberg <lw223cq@student.lnu.se>

--- Terraform/build/trigger.tf
+++ Terraform/build/trigger.tf
@@ -17,3 +17,16 @@
 #   ]
 
 # }
+
+
+resource "google_cloudbuild_trigger" "repo-trigger" { 
+  location = var.region
+  name = "${var.project_name}-trigger"
+  repository_event_config {
+    repository = google_cloudbuildv2_repository.github.id
+    push {
+      branch = "main"
+    }
+  }
+  filename = "cloudbuild.yaml"
+}
