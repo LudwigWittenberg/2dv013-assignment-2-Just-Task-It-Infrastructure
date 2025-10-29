018f262c8042dcc043c2a242e313c4794affd38b
Ludwig Wittenberg <lw223cq@student.lnu.se>
2025-10-29 11:45:07 +0100

    Removed output
    Signed-off-by: Ludwig Wittenberg <lw223cq@student.lnu.se>

--- Terraform/cluster/output.tf
+++
@@ -1,6 +0,0 @@
-output "cluster_ip_addresses" {
-  description = "The IP addresses of the clusters"
-  value = {
-    for cluster in google_container_cluster.default : cluster.name => cluster.ip_address
-  }
-}
