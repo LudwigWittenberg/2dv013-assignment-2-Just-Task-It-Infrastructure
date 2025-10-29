f1cef69295260e524445bd199af467efe5dd030a
Ludwig Wittenberg <lw223cq@student.lnu.se>
2025-10-29 18:29:56 +0100

    wip
    Signed-off-by: Ludwig Wittenberg <lw223cq@student.lnu.se>

--- Terraform/build/trigger.tf
+++ Terraform/build/trigger.tf
@@ -1,11 +1,14 @@
 resource "google_cloudbuild_trigger" "github-trigger" {
+  project           = var.project_id
   location          = var.region
   name              = "${var.project_name}-trigger"
   repository_event_config {
     repository = google_cloudbuildv2_repository.github.id
     push {
-      branch = "^main$"
+      branch = "main"
     }
   }
   filename = "cloudbuild.yaml"
+
+  depends_on = [google_cloudbuildv2_repository.github]
 }
